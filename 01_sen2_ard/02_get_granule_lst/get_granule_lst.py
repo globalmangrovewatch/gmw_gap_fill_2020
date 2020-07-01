5878e320f1dd810469592372ec901146cbdd5aea
Pete Bunting <petebunting@mac.com>
2020-07-01 16:03:55 +0100

    Add granule list.

--- 01_sen2_ard/02_get_granule_lst/get_granule_lst.py
+++ 01_sen2_ard/02_get_granule_lst/get_granule_lst.py
@@ -6,3 +6,5 @@ vec_file = '../sen2_roi_granule_vec.geojson'
 granules = rsgislib.vectorutils.readVecColumn(vec_file, 'sen2_roi_granule_vec', 'Name')
 print(granules)
 
+rsgis_utils = rsgislib.RSGISPyUtils()
+rsgis_utils.writeList2File(granules, '../sen2_roi_granule_lst.txt')
