1bd2e2cc0a63790d26e09133e7c312ce7495157a
Pete Bunting <petebunting@mac.com>
2020-07-01 21:04:43 +0100

    Update to use google API rather than command line tool.

--- 01_sen2_ard/03_find_dwnld_scns/gen_dwnld_jobs.py
+++ 01_sen2_ard/03_find_dwnld_scns/gen_dwnld_jobs.py
@@ -60,6 +60,7 @@ class FindSen2ScnsGenDwnlds(PBPTGenQProcessToolCmds):
                             c_dict['downpath'] = os.path.join(kwargs['dwnld_path'], row[0])
                             c_dict['scn_db_file'] = kwargs['scn_db_file']
                             c_dict['goog_key_json'] = kwargs['goog_key_json']
+                            c_dict['goog_proj'] = kwargs['goog_proj']
                             if not os.path.exists(c_dict['downpath']):
                                 os.mkdir(c_dict['downpath'])
                             self.params.append(c_dict)
@@ -72,6 +73,7 @@ class FindSen2ScnsGenDwnlds(PBPTGenQProcessToolCmds):
                             c_dict['downpath'] = os.path.join(kwargs['dwnld_path'], row[0])
                             c_dict['scn_db_file'] = kwargs['scn_db_file']
                             c_dict['goog_key_json'] = kwargs['goog_key_json']
+                            c_dict['goog_proj'] = kwargs['goog_proj']
                             if not os.path.exists(c_dict['downpath']):
                                 os.mkdir(c_dict['downpath'])
                             self.params.append(c_dict)
@@ -89,6 +91,7 @@ class FindSen2ScnsGenDwnlds(PBPTGenQProcessToolCmds):
                     c_dict['downpath'] = os.path.join(kwargs['dwnld_path'], scn.product_id)
                     c_dict['scn_db_file'] = kwargs['scn_db_file']
                     c_dict['goog_key_json'] = kwargs['goog_key_json']
+                    c_dict['goog_proj'] = kwargs['goog_proj']
                     if not os.path.exists(c_dict['downpath']):
                         os.mkdir(c_dict['downpath'])
                     self.params.append(c_dict)
@@ -104,7 +107,8 @@ class FindSen2ScnsGenDwnlds(PBPTGenQProcessToolCmds):
                               n_scns_xt=20,
                               scn_db_file='/scratch/a.pfb/gmw_v2_gapfill/scripts/01_sen2_ard/03_find_dwnld_scns/sen2_scn.db',
                               dwnld_path='/scratch/a.pfb/gmw_v2_gapfill/data/dwnlds',
-                              goog_key_json='/home/a.pfb/eodd_gmw_info/GlobalMangroveWatch-74b58b05fd73.json')
+                              goog_key_json='/home/a.pfb/eodd_gmw_info/GlobalMangroveWatch-74b58b05fd73.json',
+                              goog_proj='globalmangrovewatch-164501')
         self.pop_params_db()
         self.create_slurm_sub_sh("dwnld_sen2_scns", 8224, '/scratch/a.pfb/gmw_v2_gapfill/logs',
                                  run_script='run_exe_analysis.sh', job_dir="job_scripts",
--- 01_sen2_ard/03_find_dwnld_scns/perform_dwnld_jobs.py
+++ 01_sen2_ard/03_find_dwnld_scns/perform_dwnld_jobs.py
@@ -1,7 +1,7 @@
 from pbprocesstools.pbpt_q_process import PBPTQProcessTool
 from sen2scnprocess import RecordSen2Process
 import logging
-import subprocess
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -14,16 +14,51 @@ class PerformScnDownload(PBPTQProcessTool):
         sen2_rcd_obj = RecordSen2Process(self.params['scn_db_file'])
         downloaded = sen2_rcd_obj.is_scn_downloaded(self.params['product_id'])
         if not downloaded:
-            auth_cmd = "gcloud auth activate-service-account --key-file={}".format(self.params['goog_key_json'])
-            cmd = "gsutil cp -r {} {}".format(self.params['scn_url'], self.params['downpath'])
-            logger.debug("Running command: '{}'".format(auth_cmd))
-            subprocess.call(auth_cmd, shell=True)
-            logger.debug("Running command: '{}'".format(cmd))
-            subprocess.call(cmd, shell=True)
+            logger.debug("Using Google storage API to download.")
+            os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = self.params['goog_key_json']
+            os.environ["GOOGLE_CLOUD_PROJECT"] = self.params['goog_proj']
+            from google.cloud import storage
+            storage_client = storage.Client()
+
+            logger.debug("Building download info for '{}'".format(self.params['scn_url']))
+            url_path = self.params['scn_url']
+            url_path = url_path.replace("gs://", "")
+            url_path_parts = url_path.split("/")
+            bucket_name = url_path_parts[0]
+            if bucket_name != "gcp-public-data-sentinel-2":
+                logger.error("Incorrect bucket name '" + bucket_name + "'")
+                raise Exception("The bucket specified in the URL is not the Google Public Landsat Bucket"
+                                          " - something has gone wrong.")
+            bucket_prefix = url_path.replace(bucket_name + "/", "")
+            dwnld_out_dirname = url_path_parts[-1]
+            scn_lcl_dwnld_path = os.path.join(self.params['downpath'], dwnld_out_dirname)
+            if not os.path.exists(scn_lcl_dwnld_path):
+                os.mkdir(scn_lcl_dwnld_path)
+
+            logger.debug("Get the storage bucket and blob objects.")
+            bucket_obj = storage_client.get_bucket(bucket_name)
+            bucket_blobs = bucket_obj.list_blobs(prefix=bucket_prefix)
+            scn_dwnlds_filelst = list()
+            for blob in bucket_blobs:
+                if "$folder$" in blob.name:
+                    continue
+                scnfilename = blob.name.replace(bucket_prefix + "/", "")
+                dwnld_file = os.path.join(scn_lcl_dwnld_path, scnfilename)
+                dwnld_dirpath = os.path.split(dwnld_file)[0]
+                if (not os.path.exists(dwnld_dirpath)):
+                    os.makedirs(dwnld_dirpath, exist_ok=True)
+                scn_dwnlds_filelst.append({"bucket_path": blob.name, "dwnld_path": dwnld_file})
+
+
+            bucket_obj = storage_client.get_bucket(bucket_name)
+            for dwnld in scn_dwnlds_filelst:
+                blob_obj = bucket_obj.blob(dwnld["bucket_path"])
+                blob_obj.download_to_filename(dwnld["dwnld_path"])
+
             sen2_rcd_obj.set_scn_downloaded(self.params['product_id'], self.params['downpath'])
 
     def required_fields(self, **kwargs):
-        return ["product_id", "scn_url", "downpath", "scn_db_file", "goog_key_json"]
+        return ["product_id", "scn_url", "downpath", "scn_db_file", "goog_key_json", "goog_proj"]
 
     def outputs_present(self, **kwargs):
         sen2_rcd_obj = RecordSen2Process(self.params['scn_db_file'])
