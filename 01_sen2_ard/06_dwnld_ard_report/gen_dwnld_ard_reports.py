a0b55877357ae79fed8413fe449daa35957acb18
Pete Bunting <petebunting@mac.com>
2020-07-03 15:29:29 +0100

    Update add scripts to report scenes haven't processed to ARD or downloaded.

---
+++ 01_sen2_ard/06_dwnld_ard_report/gen_dwnld_ard_reports.py
@@ -0,0 +1,17 @@
+sys.path.insert(0, "../03_find_dwnld_scns")
+from sen2scnprocess import RecordSen2Process
+
+
+sen2_rcd_obj = RecordSen2Process("/scratch/a.pfb/gmw_v2_gapfill/scripts/01_sen2_ard/03_find_dwnld_scns/sen2_scn.db")
+
+print("Scenes to download:")
+scns = sen2_rcd_obj.get_scns_download()
+for scn in scns:
+    print("\t{}".format(scn.product_id))
+
+print("Scenes to ARD:")
+scns = sen2_rcd_obj.get_scns_ard()
+for scn in scns:
+    print("\t{}".format(scn.product_id))
+
+
