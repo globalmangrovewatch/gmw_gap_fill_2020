ec3f47e23f2f9b03b3c648ff6b57616a935c7ff3
Pete Bunting <petebunting@mac.com>
2020-07-07 16:34:16 +0100

    Update with function which uses geopandas to merge the input vectors.

--- 01_sen2_ard/09_gen_sen2_vld_msks/merge_valid_msks.py
+++ 01_sen2_ard/09_gen_sen2_vld_msks/merge_valid_msks.py
@@ -1,7 +1,45 @@
 import glob
 import rsgislib.vectorutils
+import geopandas
+
+def merge_vector_files(input_files, output_file, output_lyr=None, out_format='GPKG', out_epsg=None):
+    """
+    A function which merges the input files into a single output file. If the input files have multiple
+    layers they are all merged into the output file.
+
+    :param input_files: list of input files
+    :param output_file: output vector file.
+    :param output_lyr: output vector layer.
+    :param out_format: output file format.
+    :param out_epsg: if input layers are different projections then option can be used to define the output
+                     projection.
+
+    """
+    first = True
+    for vec_file in input_files:
+        lyrs = rsgislib.vectorutils.getVecLyrsLst(vec_file)
+        for lyr in lyrs:
+            if first:
+                data_gdf = geopandas.read_file(vec_file, layer=lyr)
+                if out_epsg is not None:
+                    data_gdf = data_gdf.set_crs(epsg=out_epsg)
+                first = False
+            else:
+                tmp_data_gdf = geopandas.read_file(vec_file, layer=lyr)
+                if out_epsg is not None:
+                    tmp_data_gdf = tmp_data_gdf.set_crs(epsg=out_epsg)
+
+                data_gdf.append(tmp_data_gdf)
+
+    if out_format == "GPKG":
+        if output_lyr is None:
+            raise Exception("If output format is GPKG then an output layer is required.")
+        data_gdf.to_file(output_file, layer=output_lyr, driver=out_format)
+    else:
+        data_gdf.to_file(output_file, driver=out_format)
+
 
 input_vecs = glob.glob("/scratch/a.pfb/gmw_v2_gapfill/data/granule_vld_msks/*.gpkg")
+merge_vector_files(input_vecs, '/scratch/a.pfb/gmw_v2_gapfill/data/granule_vld_msks.gpkg', 'GPKG', out_epsg=4326)
 
-rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, '/scratch/a.pfb/gmw_v2_gapfill/data/granule_vld_msks.gpkg', 'granule_vld_msks', False)
 
