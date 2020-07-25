981e1b491c52011b5f8196e9ad23b85ef595729f
Pete Bunting <petebunting@mac.com>
2020-07-25 21:02:37 +0100

    Update to run the appropriate merge function.

--- 01_sen2_ard/09_gen_sen2_vld_msks/run_merge_valid_msks.sh
+++ 01_sen2_ard/09_gen_sen2_vld_msks/run_merge_valid_msks.sh
@@ -1,1 +1,6 @@
-singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_valid_msks.py
+singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_utm_to_wgs84.py
+
+
+#singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python merge_valid_msks.py
+
+
