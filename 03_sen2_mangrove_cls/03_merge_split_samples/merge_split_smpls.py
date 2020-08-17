0f2c594361a05eb99252f6cdd856eb156fb75375
Pete Bunting <petebunting@mac.com>
2020-08-17 21:14:02 +0100

    Mask values so they are finite and within failed range.

--- 03_sen2_mangrove_cls/03_merge_split_samples/merge_split_smpls.py
+++ 03_sen2_mangrove_cls/03_merge_split_samples/merge_split_smpls.py
@@ -4,6 +4,36 @@ import tqdm
 import rsgislib.imageutils
 import rsgislib.classification
 
+
+def msk_to_finite_values(input_h5, output_h5, datatype=None, lower_limit=None, upper_limit=None):
+    import h5py
+    import numpy
+
+    rsgis_utils = rsgislib.RSGISPyUtils()
+    if datatype is None:
+        datatype = rsgislib.TYPE_32FLOAT
+    h5_dtype = rsgis_utils.getNumpyCharCodesDataType(datatype)
+
+    fH5 = h5py.File(input_h5, 'r')
+    data_shp = fH5['DATA/DATA'].shape
+    num_vars = data_shp[1]
+    data = numpy.array(fH5['DATA/DATA'])
+    data = data[numpy.isfinite(data).all(axis=1)]
+    if lower_limit is not None:
+        data = data[numpy.any(data > lower_limit, axis=1)]
+    if upper_limit is not None:
+        data = data[numpy.any(data < upper_limit, axis=1)]
+
+    fH5Out = h5py.File(output_h5, 'w')
+    dataGrp = fH5Out.create_group("DATA")
+    metaGrp = fH5Out.create_group("META-DATA")
+    dataGrp.create_dataset('DATA', data=data, chunks=(1000, num_vars), compression="gzip",
+                           shuffle=True, dtype=h5_dtype)
+    describDS = metaGrp.create_dataset("DESCRIPTION", (1,), dtype="S10")
+    describDS[0] = 'finite values'.encode()
+    fH5Out.close()
+
+
 mng_files = glob.glob('/scratch/a.pfb/gmw_v2_gapfill/data/scn_h5_samples/*mng_smpls.h5')
 oth_files = glob.glob('/scratch/a.pfb/gmw_v2_gapfill/data/scn_h5_samples/*oth_smpls.h5')
 
@@ -12,6 +42,14 @@ oth_all_samples = '/scratch/a.pfb/gmw_v2_gapfill/data/oth_smpls_all.h5'
 
 rsgislib.imageutils.mergeExtractedHDF5Data(mng_files, mng_all_samples)
 rsgislib.imageutils.mergeExtractedHDF5Data(oth_files, oth_all_samples)
+
+
+mng_all_mskd_samples = '/scratch/a.pfb/gmw_v2_gapfill/data/mng_smpls_all_mskd.h5'
+oth_all_mskd_samples = '/scratch/a.pfb/gmw_v2_gapfill/data/oth_smpls_all_mskd.h5'
+
+msk_to_finite_values(mng_all_samples, mng_all_mskd_samples, datatype=rsgislib.TYPE_16UINT, lower_limit=0, upper_limit=1000)
+msk_to_finite_values(oth_all_samples, oth_all_mskd_samples, datatype=rsgislib.TYPE_16UINT, lower_limit=0, upper_limit=1000)
+
 
 samples_dir = '/scratch/a.pfb/gmw_v2_gapfill/data/set_samples_h5'
 n_samples = 1000
@@ -23,7 +61,7 @@ n_opt_smpl = 20 # 20% sample used for optimisation.
 
 for i in tqdm.tqdm(range(n_sets)):
     mng_smps_file = os.path.join(samples_dir, "mng_samples_{}.h5".format(i+1))
-    rsgislib.imageutils.randomSampleHDF5File(mng_all_samples, mng_smps_file, n_samples, i)
+    rsgislib.imageutils.randomSampleHDF5File(mng_all_mskd_samples, mng_smps_file, n_samples, i)
 
     mng_train_smps_file = os.path.join(samples_dir, "mng_train_samples_{}.h5".format(i + 1))
     mng_valid_smps_file = os.path.join(samples_dir, "mng_valid_samples_{}.h5".format(i + 1))
@@ -37,7 +75,7 @@ for i in tqdm.tqdm(range(n_sets)):
                                              int(n_train_smpls / n_opt_smpl), 42)
 
     oth_smps_file = os.path.join(samples_dir, "oth_samples_{}.h5".format(i + 1))
-    rsgislib.imageutils.randomSampleHDF5File(oth_all_samples, oth_smps_file, n_samples, i)
+    rsgislib.imageutils.randomSampleHDF5File(oth_all_mskd_samples, oth_smps_file, n_samples, i)
 
     oth_train_smps_file = os.path.join(samples_dir, "oth_train_samples_{}.h5".format(i + 1))
     oth_valid_smps_file = os.path.join(samples_dir, "oth_valid_samples_{}.h5".format(i + 1))
