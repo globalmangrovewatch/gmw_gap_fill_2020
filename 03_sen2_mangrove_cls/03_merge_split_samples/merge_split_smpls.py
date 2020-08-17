c35a2b980cadfa1b84fe9f0b1051b51f53b8a6c1
Pete Bunting <petebunting@mac.com>
2020-08-17 13:08:16 +0100

    Update to take a sample for optimisation parameters.

--- 03_sen2_mangrove_cls/03_merge_split_samples/merge_split_smpls.py
+++ 03_sen2_mangrove_cls/03_merge_split_samples/merge_split_smpls.py
@@ -19,25 +19,33 @@ n_test_smpls = 250
 n_valid_smpls = 250
 n_train_smpls = 500
 n_sets = 100
+n_opt_smpl = 20 # 20% sample used for optimisation.
 
 for i in tqdm.tqdm(range(n_sets)):
     mng_smps_file = os.path.join(samples_dir, "mng_samples_{}.h5".format(i+1))
     rsgislib.imageutils.randomSampleHDF5File(mng_all_samples, mng_smps_file, n_samples, i)
 
     mng_train_smps_file = os.path.join(samples_dir, "mng_train_samples_{}.h5".format(i + 1))
-    mng_test_smps_file = os.path.join(samples_dir, "mng_test_samples_{}.h5".format(i + 1))
     mng_valid_smps_file = os.path.join(samples_dir, "mng_valid_samples_{}.h5".format(i + 1))
+    mng_test_smps_file = os.path.join(samples_dir, "mng_test_samples_{}.h5".format(i + 1))
     rsgislib.classification.split_sample_train_valid_test(mng_smps_file, mng_train_smps_file, mng_valid_smps_file,
                                                           mng_test_smps_file, n_test_smpls, n_valid_smpls,
                                                           n_train_smpls, rand_seed=42, datatype=rsgislib.TYPE_16UINT)
+
+    mng_train_smps_opt_file = os.path.join(samples_dir, "mng_train_samples_{}_opt.h5".format(i + 1))
+    rsgislib.imageutils.randomSampleHDF5File(mng_train_smps_file, mng_train_smps_opt_file,
+                                             int(n_train_smpls / n_opt_smpl), 42)
 
     oth_smps_file = os.path.join(samples_dir, "oth_samples_{}.h5".format(i + 1))
     rsgislib.imageutils.randomSampleHDF5File(oth_all_samples, oth_smps_file, n_samples, i)
 
     oth_train_smps_file = os.path.join(samples_dir, "oth_train_samples_{}.h5".format(i + 1))
-    oth_test_smps_file = os.path.join(samples_dir, "oth_test_samples_{}.h5".format(i + 1))
     oth_valid_smps_file = os.path.join(samples_dir, "oth_valid_samples_{}.h5".format(i + 1))
+    oth_test_smps_file = os.path.join(samples_dir, "oth_test_samples_{}.h5".format(i + 1))
     rsgislib.classification.split_sample_train_valid_test(oth_smps_file, oth_train_smps_file, oth_valid_smps_file,
                                                           oth_test_smps_file, n_test_smpls, n_valid_smpls,
                                                           n_train_smpls, rand_seed=42, datatype=rsgislib.TYPE_16UINT)
 
+    oth_train_smps_opt_file = os.path.join(samples_dir, "oth_train_samples_{}_opt.h5".format(i + 1))
+    rsgislib.imageutils.randomSampleHDF5File(oth_train_smps_file, oth_train_smps_opt_file,
+                                             int(n_train_smpls / n_opt_smpl), 42)
