b5532206b41e30298f5eecb51ed4e6ae67b625a4
Pete Bunting <petebunting@mac.com>
2020-08-17 11:29:31 +0100

    Fix file names.

--- 03_sen2_mangrove_cls/03_merge_split_samples/merge_split_smpls.py
+++ 03_sen2_mangrove_cls/03_merge_split_samples/merge_split_smpls.py
@@ -25,8 +25,8 @@ for i in tqdm.tqdm(range(n_sets)):
     rsgislib.imageutils.randomSampleHDF5File(mng_all_samples, mng_smps_file, n_samples, i)
 
     mng_train_smps_file = os.path.join(samples_dir, "mng_train_samples_{}.h5".format(i + 1))
-    mng_test_smps_file = os.path.join(samples_dir, "mng_train_samples_{}.h5".format(i + 1))
-    mng_valid_smps_file = os.path.join(samples_dir, "mng_train_samples_{}.h5".format(i + 1))
+    mng_test_smps_file = os.path.join(samples_dir, "mng_test_samples_{}.h5".format(i + 1))
+    mng_valid_smps_file = os.path.join(samples_dir, "mng_valid_samples_{}.h5".format(i + 1))
     rsgislib.classification.split_sample_train_valid_test(mng_smps_file, mng_train_smps_file, mng_valid_smps_file,
                                                           mng_test_smps_file, n_test_smpls, n_valid_smpls,
                                                           n_train_smpls, rand_seed=42, datatype=rsgislib.TYPE_16UINT)
@@ -35,8 +35,8 @@ for i in tqdm.tqdm(range(n_sets)):
     rsgislib.imageutils.randomSampleHDF5File(oth_all_samples, oth_smps_file, n_samples, i)
 
     oth_train_smps_file = os.path.join(samples_dir, "oth_train_samples_{}.h5".format(i + 1))
-    oth_test_smps_file = os.path.join(samples_dir, "oth_train_samples_{}.h5".format(i + 1))
-    oth_valid_smps_file = os.path.join(samples_dir, "oth_train_samples_{}.h5".format(i + 1))
+    oth_test_smps_file = os.path.join(samples_dir, "oth_test_samples_{}.h5".format(i + 1))
+    oth_valid_smps_file = os.path.join(samples_dir, "oth_valid_samples_{}.h5".format(i + 1))
     rsgislib.classification.split_sample_train_valid_test(oth_smps_file, oth_train_smps_file, oth_valid_smps_file,
                                                           oth_test_smps_file, n_test_smpls, n_valid_smpls,
                                                           n_train_smpls, rand_seed=42, datatype=rsgislib.TYPE_16UINT)
