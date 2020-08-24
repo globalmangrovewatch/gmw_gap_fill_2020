48dab78e3345ea503f4734312b63e64452941ecf
Pete Bunting <petebunting@mac.com>
2020-08-24 21:27:34 +0100

    Update to run whole analysis.

--- 03_sen2_mangrove_cls/01_define_training/merge_train_samples.py
+++ 03_sen2_mangrove_cls/01_define_training/merge_train_samples.py
@@ -3,7 +3,7 @@ import glob
 
 input_files = glob.glob("/scratch/a.pfb/gmw_v2_gapfill/data/granule_mang_train_vecs/*.gpkg")
 output_file = "/scratch/a.pfb/gmw_v2_gapfill/data/granule_mang_train_smpls.gpkg"
-#rsgislib.vectorutils.merge_vector_files(input_files, output_file, output_lyr="samples", out_format='GPKG', out_epsg=4326)
+rsgislib.vectorutils.merge_vector_files(input_files, output_file, output_lyr="samples", out_format='GPKG', out_epsg=4326)
 
 def add_numeric_col_lut(vec_file, vec_lyr, ref_col, val_lut, out_col, vec_out_file, vec_out_lyr, out_format='GPKG'):
     """
