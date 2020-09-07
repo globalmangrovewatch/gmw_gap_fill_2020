1c40b6dca9b8f71266754415b9505df2a20a8566
Pete Bunting <petebunting@mac.com>
2020-09-07 11:18:05 +0100

    Add scripts for merging granule classifications.

---
+++ 03_sen2_mangrove_cls/09_merge_granule_cls/gen_merge_granule_cls_cmds.py
@@ -0,0 +1,55 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+import sys
+import rsgislib
+import glob
+
+logger = logging.getLogger(__name__)
+
+class GenMergeGranuleClsCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs['scn_db_file']):
+            raise Exception("Sentinel-2 scene database does not exist...")
+
+        rsgis_utils = rsgislib.RSGISPyUtils()
+        granule_lst = rsgis_utils.readTextFile2List(kwargs['granule_lst'])
+
+        for granule in granule_lst:
+            print(granule)
+            for thres in ['cls_25', 'cls_50', 'cls_75']:
+                cls_files = glob.glob(os.path.join(kwargs['cls_scn_dir'], "*{}*{}*.kea".format(granule, thres)))
+                if len(cls_files) > 0:
+                    c_dict = dict()
+                    c_dict['granule'] = granule
+                    c_dict['cls_files'] = cls_files
+                    c_dict['out_sum_cls_file'] = os.path.join(kwargs['out_granule_dir'], "{}_{}_sum_cls.kea".format(granule, thres))
+                    c_dict['out_cls_25_file'] = os.path.join(kwargs['out_granule_dir'], "{}_{}_cls_25.kea".format(granule, thres))
+                    c_dict['out_cls_50_file'] = os.path.join(kwargs['out_granule_dir'], "{}_{}_cls_50.kea".format(granule, thres))
+                    c_dict['out_cls_75_file'] = os.path.join(kwargs['out_granule_dir'], "{}_{}_cls_75.kea".format(granule, thres))
+                    if not (os.path.exists(c_dict['out_sum_cls_file']) and os.path.exists(c_dict['out_cls_25_file']) and os.path.exists(c_dict['out_cls_50_file']) and os.path.exists(c_dict['out_cls_75_file'])):
+                        self.params.append(c_dict)
+
+    def run_gen_commands(self):
+        self.gen_command_info(granule_lst='/scratch/a.pfb/gmw_v2_gapfill/scripts/01_sen2_ard/sen2_roi_granule_lst.txt',
+                              cls_scn_dir='/scratch/a.pfb/gmw_v2_gapfill/data/sum_scn_cls_files',
+                              out_granule_dir='/scratch/a.pfb/gmw_v2_gapfill/data/sum_scn_granule_files')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("merge_granule_cls", 16448, '/scratch/a.pfb/gmw_v2_gapfill/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("merge_granule_cls.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'merge_granule_cls'
+    process_tools_cls = 'MergeGranuleCls'
+
+    create_tools = GenMergeGranuleClsCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                      lock_file_path="./gmw_gapfill_lock_file.txt",
+                                      process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
---
+++ 03_sen2_mangrove_cls/09_merge_granule_cls/merge_granule_cls.py
@@ -0,0 +1,90 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+class MergeGranuleCls(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='merge_granule_cls.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        import rsgislib
+        import rsgislib.imagecalc
+        import rsgislib.imageutils
+        import rsgislib.rastergis
+        rsgis_utils = rsgislib.RSGISPyUtils()
+
+        cls_files = []
+        for cls_file in self.params['cls_files']:
+            if os.path.exists(cls_file):
+                try:
+                    print(cls_file)
+                    n_bands = rsgis_utils.getImageBandCount(cls_file)
+                    if n_bands == 1:
+                        cls_files.append(cls_file)
+                except:
+                    print("REMOVING: {}".format(cls_file))
+                    os.remove(cls_file)
+
+        try:
+            rsgislib.imagecalc.calcMultiImgBandStats(cls_files, self.params['out_sum_cls_file'],
+                                                     rsgislib.SUMTYPE_MEAN, 'KEA', rsgislib.TYPE_32FLOAT, -1, True)
+        except Exception as e:
+            for img in cls_files:
+                print(img)
+                print(rsgis_utils.getImageRes(img))
+                print(rsgis_utils.getImageBandCount(img))
+            print(e)
+            raise e
+        rsgislib.imageutils.popImageStats(self.params['out_sum_cls_file'], usenodataval=True, nodataval=0, calcpyramids=True)
+
+        band_defns = list()
+        band_defns.append(rsgislib.imagecalc.BandDefn('cld', self.params['clr_sky'], 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('scr', self.params['out_sum_cls_file'], 1))
+        rsgislib.imagecalc.bandMath(self.params['out_cls_25_file'], "cld==0?255:scr>0.3?1:0", 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(self.params['out_cls_25_file'], addclrtab=True, calcpyramids=True, ignorezero=False)
+
+        band_defns = list()
+        band_defns.append(rsgislib.imagecalc.BandDefn('cld', self.params['clr_sky'], 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('scr', self.params['out_sum_cls_file'], 1))
+        rsgislib.imagecalc.bandMath(self.params['out_cls_50_file'], "cld==0?255:scr>0.5?1:0", 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(self.params['out_cls_50_file'], addclrtab=True, calcpyramids=True, ignorezero=False)
+
+        band_defns = list()
+        band_defns.append(rsgislib.imagecalc.BandDefn('cld', self.params['clr_sky'], 1))
+        band_defns.append(rsgislib.imagecalc.BandDefn('scr', self.params['out_sum_cls_file'], 1))
+        rsgislib.imagecalc.bandMath(self.params['out_cls_75_file'], "cld==0?255:scr>0.8?1:0", 'KEA', rsgislib.TYPE_8UINT, band_defns)
+        rsgislib.rastergis.populateStats(self.params['out_cls_75_file'], addclrtab=True, calcpyramids=True, ignorezero=False)
+
+
+    def required_fields(self, **kwargs):
+        return ["scn_id", "cls_files", "clr_sky", "out_sum_cls_file", "out_cls_25_file", "out_cls_50_file", "out_cls_75_file"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_sum_cls_file']] = 'gdal_img'
+        files_dict[self.params['out_cls_25_file']] = 'gdal_img'
+        files_dict[self.params['out_cls_50_file']] = 'gdal_img'
+        files_dict[self.params['out_cls_75_file']] = 'gdal_img'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output files.
+        if os.path.exists(self.params['out_sum_cls_file']):
+            os.remove(self.params['out_sum_cls_file'])
+
+        if os.path.exists(self.params['out_cls_25_file']):
+            os.remove(self.params['out_cls_25_file'])
+
+        if os.path.exists(self.params['out_cls_50_file']):
+            os.remove(self.params['out_cls_50_file'])
+
+        if os.path.exists(self.params['out_cls_75_file']):
+            os.remove(self.params['out_cls_75_file'])
+
+if __name__ == "__main__":
+    MergeGranuleCls().std_run()
+
+
