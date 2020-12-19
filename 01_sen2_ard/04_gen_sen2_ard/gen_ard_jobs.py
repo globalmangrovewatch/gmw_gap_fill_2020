c391e8255d8a7bd5770189a9b05ef6e60b55d344
Pete Bunting <petebunting@mac.com>
2020-12-19 15:22:36 +0000

    Update to worked with latest version of pb_process_tools

--- 01_sen2_ard/04_gen_sen2_ard/gen_ard_jobs.py
+++ 01_sen2_ard/04_gen_sen2_ard/gen_ard_jobs.py
@@ -41,23 +41,14 @@ class FindSen2ScnsGenDwnlds(PBPTGenQProcessToolCmds):
                                  job_time_limit='2-23:59',
                                  module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
 
-    def run_check_outputs(self):
-        process_tools_mod = 'perform_ard_process'
-        process_tools_cls = 'PerformScnARD'
-        time_sample_str = self.generate_readable_timestamp_str()
-        out_err_file = 'processing_errs_{}.txt'.format(time_sample_str)
-        out_non_comp_file = 'non_complete_errs_{}.txt'.format(time_sample_str)
-        self.check_job_outputs(process_tools_mod, process_tools_cls, out_err_file, out_non_comp_file)
-
-    def run_remove_outputs(self, all_jobs=False, error_jobs=False):
-        process_tools_mod = 'create_granule_veg_msk'
-        process_tools_cls = 'CreateGranuleVegMsk'
-        self.remove_job_outputs(process_tools_mod, process_tools_cls, all_jobs, error_jobs)
-
-
 if __name__ == "__main__":
     py_script = os.path.abspath("perform_ard_process.py")
     script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
 
-    create_tools = FindSen2ScnsGenDwnlds(cmd=script_cmd, sqlite_db_file="ard_sen2_scns.db")
+    process_tools_mod = 'perform_ard_process'
+    process_tools_cls = 'PerformScnARD'
+
+    create_tools = FindSen2ScnsGenDwnlds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                        lock_file_path="./gmw_gapfill_lock_file.txt",
+                                        process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
     create_tools.parse_cmds()
