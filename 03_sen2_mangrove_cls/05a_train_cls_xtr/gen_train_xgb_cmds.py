00a891b106a3e3967baf6cb109311a582897f7f6
Pete Bunting <petebunting@mac.com>
2020-12-30 15:53:11 +0000

    Add scripts to further train the classifiers.

---
+++ 03_sen2_mangrove_cls/05a_train_cls_xtr/gen_train_xgb_cmds.py
@@ -0,0 +1,58 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+class GenExtractSamplesCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        for i in range(kwargs['n_sample_sets']):
+            mng_train_smps_file = os.path.join(kwargs['samples_dir'], "mng_train_samples_xtr_{}.h5".format(i + 1))
+            mng_valid_smps_file = os.path.join(kwargs['samples_dir'], "mng_valid_samples_{}.h5".format(i + 1))
+            mng_test_smps_file = os.path.join(kwargs['samples_dir'], "mng_test_samples_xtr_{}.h5".format(i + 1))
+
+            oth_train_smps_file = os.path.join(kwargs['samples_dir'], "oth_train_samples_xtr_{}_opt.h5".format(i + 1))
+            oth_valid_smps_file = os.path.join(kwargs['samples_dir'], "oth_valid_samples_{}.h5".format(i + 1))
+            oth_test_smps_file = os.path.join(kwargs['samples_dir'], "oth_test_samples_xtr_{}.h5".format(i + 1))
+
+            cls_params_file = os.path.join(kwargs['out_cls_dir'], 'sen2_gfill_opt_xgb_cls_{}.json'.format(i + 1))
+            cls_mdl_file = os.path.join(kwargs['out_cls_dir'], 'sen2_gfill_opt_xgb_cls_trained_{}.mdl'.format(i + 1))
+            out_cls_file = os.path.join(kwargs['out_cls_dir'], 'sen2_gfill_opt_xgb_cls_trained__xtr{}.mdl'.format(i + 1))
+
+            c_dict = dict()
+            c_dict['mng_train_smps_file'] = mng_train_smps_file
+            c_dict['mng_valid_smps_file'] = mng_valid_smps_file
+            c_dict['mng_test_smps_file'] = mng_test_smps_file
+            c_dict['oth_train_smps_file'] = oth_train_smps_file
+            c_dict['oth_valid_smps_file'] = oth_valid_smps_file
+            c_dict['oth_test_smps_file'] = oth_test_smps_file
+            c_dict['cls_params_file'] = cls_params_file
+            c_dict['cls_mdl_file'] = cls_mdl_file
+            c_dict['out_cls_file'] = out_cls_file
+            self.params.append(c_dict)
+
+    def run_gen_commands(self):
+        self.gen_command_info(samples_dir='/scratch/a.pfb/gmw_v2_gapfill/data/set_samples_h5',
+                              n_sample_sets=100,
+                              cls_params_dir='/scratch/a.pfb/gmw_v2_gapfill/data/opt_cls_files',
+                              out_cls_dir='/scratch/a.pfb/gmw_v2_gapfill/data/opt_cls_files')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("train_xgb_cls", 16448, '/scratch/a.pfb/gmw_v2_gapfill/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("train_xgb_cls.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'train_xgb_cls'
+    process_tools_cls = 'TrainXGBParams'
+
+    create_tools = GenExtractSamplesCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                          lock_file_path="./gmw_gapfill_lock_file.txt",
+                                          process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
---
+++ 03_sen2_mangrove_cls/05a_train_cls_xtr/train_xgb_cls.py
@@ -0,0 +1,46 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import rsgislib
+import rsgislib.classification
+import rsgislib.classification.classxgboost
+
+logger = logging.getLogger(__name__)
+
+
+class TrainXGBParams(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='train_xgb_cls.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        rsgislib.classification.classxgboost.train_xgboost_binary_classifer(self.params['out_cls_file'],
+                                                                            self.params['cls_params_file'],
+                                                                            self.params['mng_train_smps_file'],
+                                                                            self.params['mng_valid_smps_file'],
+                                                                            self.params['mng_test_smps_file'],
+                                                                            self.params['oth_train_smps_file'],
+                                                                            self.params['oth_valid_smps_file'],
+                                                                            self.params['oth_test_smps_file'],
+                                                                            nthread=1,
+                                                                            mdl_cls_obj=self.params['cls_mdl_file'])
+
+    def required_fields(self, **kwargs):
+        return ["mng_train_smps_file", "mng_valid_smps_file", "mng_test_smps_file",
+                "oth_train_smps_file", "oth_valid_smps_file", "oth_test_smps_file",
+                "cls_params_file", "cls_mdl_file", "out_cls_file"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_cls_file']] = 'file'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output file.
+        if os.path.exists(self.params['out_cls_file']):
+            os.remove(self.params['out_cls_file'])
+
+if __name__ == "__main__":
+    TrainXGBParams().std_run()
+
+
