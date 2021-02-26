5f794c912fa0bc4841dd8d3453759fecbe365109
Pete Bunting <petebunting@mac.com>
2021-02-26 23:01:53 +0000

    Update to use habitat mask v6

--- 03_sen2_mangrove_cls/01_define_training/gen_granule_train_samples.py
+++ 03_sen2_mangrove_cls/01_define_training/gen_granule_train_samples.py
@@ -38,8 +38,8 @@ class GenGranuleTrainSamples(PBPTGenQProcessToolCmds):
     def run_gen_commands(self):
         self.gen_command_info(granule_lst='/scratch/a.pfb/gmw_v2_gapfill/scripts/01_sen2_ard/sen2_roi_granule_lst.txt',
                               granule_veg_msks_dir='/scratch/a.pfb/gmw_v2_gapfill/data/granule_vegmsks_imgs',
-                              gmw_hab_msk_vec='/scratch/a.pfb/gmw_v2_gapfill/data/GMW_Mangrove_Habitat_v5.gpkg',
-                              gmw_hab_msk_lyr='gmw_hab_v5',
+                              gmw_hab_msk_vec='/scratch/a.pfb/gmw_v2_gapfill/data/GMW_Mangrove_Habitat_v6.gpkg',
+                              gmw_hab_msk_lyr='gmw_hab_v6',
                               gmw_msk_vec='/scratch/a.pfb/gmw_v2_gapfill/data/GMW_MangroveExtent_WGS84_v2.0.gpkg',
                               gmw_msk_lyr='gmw2010v2.0',
                               granule_out_img_path='/scratch/a.pfb/gmw_v2_gapfill/data/granule_mang_train_imgs',
