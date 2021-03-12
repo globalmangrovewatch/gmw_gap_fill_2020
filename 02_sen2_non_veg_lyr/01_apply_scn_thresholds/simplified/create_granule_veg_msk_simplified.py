dcfbac8e228e8f04e455b0d481f7ce76fd479e58
Pete Bunting <petebunting@mac.com>
2021-03-12 17:25:14 +0000

    Add simplified scripts to generate veg mask.

---
+++ 02_sen2_non_veg_lyr/01_apply_scn_thresholds/simplified/create_granule_veg_msk_simplified.py
@@ -0,0 +1,188 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import shutil
+import rsgislib
+import rsgislib.imagecalc
+import rsgislib.vectorutils
+import rsgislib.rastergis
+import rsgislib.imagecalc.calcindices
+import rsgislib.imageutils
+
+import osgeo.ogr as ogr
+
+logger = logging.getLogger(__name__)
+
+def unwrap_wgs84_bbox(bbox):
+    #(MinX, MaxX, MinY, MaxY)
+    bboxes = []
+    if bbox[1] < bbox[0]:
+        bbox1 = [-180.0, bbox[1], bbox[2], bbox[3]]
+        bboxes.append(bbox1)
+        bbox2 = [bbox[0], 180.0, bbox[2], bbox[3]]
+        bboxes.append(bbox2)
+    else:
+        bboxes.append(bbox)
+    return bboxes
+
+
+def resampleImage2Match(inRefImg, inProcessImg, outImg, gdalformat, interpMethod, datatype=None, noDataVal=None,
+                        multicore=False):
+    """
+A utility function to resample an existing image to the projection and/or pixel size of another image.
+
+Where:
+
+:param inRefImg: is the input reference image to which the processing image is to resampled to.
+:param inProcessImg: is the image which is to be resampled.
+:param outImg: is the output image file.
+:param gdalformat: is the gdal format for the output image.
+:param interpMethod: is the interpolation method used to resample the image [bilinear, lanczos, cubicspline, nearestneighbour, cubic, average, mode]
+:param datatype: is the rsgislib datatype of the output image (if none then it will be the same as the input file).
+:param multicore: use multiple processing cores (Default = False)
+
+"""
+    import osgeo.gdal as gdal
+
+    rsgisUtils = rsgislib.RSGISPyUtils()
+    numBands = rsgisUtils.getImageBandCount(inProcessImg)
+    if noDataVal == None:
+        noDataVal = rsgisUtils.getImageNoDataValue(inProcessImg)
+
+    if datatype == None:
+        datatype = rsgisUtils.getRSGISLibDataTypeFromImg(inProcessImg)
+
+    interpolationMethod = gdal.GRA_NearestNeighbour
+    if interpMethod == 'bilinear':
+        interpolationMethod = gdal.GRA_Bilinear
+    elif interpMethod == 'lanczos':
+        interpolationMethod = gdal.GRA_Lanczos
+    elif interpMethod == 'cubicspline':
+        interpolationMethod = gdal.GRA_CubicSpline
+    elif interpMethod == 'nearestneighbour':
+        interpolationMethod = gdal.GRA_NearestNeighbour
+    elif interpMethod == 'cubic':
+        interpolationMethod = gdal.GRA_Cubic
+    elif interpMethod == 'average':
+        interpolationMethod = gdal.GRA_Average
+    elif interpMethod == 'mode':
+        interpolationMethod = gdal.GRA_Mode
+    else:
+        raise Exception("Interpolation method was not recognised or known.")
+
+    backVal = 0.0
+    haveNoData = False
+    if noDataVal != None:
+        backVal = float(noDataVal)
+        haveNoData = True
+
+    rsgislib.imageutils.createCopyImage(inRefImg, outImg, numBands, backVal, gdalformat, datatype)
+
+    inFile = gdal.Open(inProcessImg, gdal.GA_ReadOnly)
+    outFile = gdal.Open(outImg, gdal.GA_Update)
+
+    try:
+        import tqdm
+        pbar = tqdm.tqdm(total=100)
+        callback = lambda *args, **kw: pbar.update()
+    except:
+        callback = gdal.TermProgress
+
+    wrpOpts = []
+    if multicore:
+        if haveNoData:
+            wrpOpts = gdal.WarpOptions(resampleAlg=interpolationMethod, srcNodata=noDataVal, dstNodata=noDataVal,
+                                       multithread=True, callback=callback)
+        else:
+            wrpOpts = gdal.WarpOptions(resampleAlg=interpolationMethod, multithread=True, callback=callback)
+    else:
+        if haveNoData:
+            wrpOpts = gdal.WarpOptions(resampleAlg=interpolationMethod, srcNodata=noDataVal, dstNodata=noDataVal,
+                                       multithread=False, callback=callback)
+        else:
+            wrpOpts = gdal.WarpOptions(resampleAlg=interpolationMethod, multithread=False, callback=callback)
+
+    gdal.Warp(outFile, inFile, options=wrpOpts)
+
+    inFile = None
+    outFile = None
+
+
+
+class CreateGranuleVegMsk(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='perform_coverage_chk.py', descript=None)
+
+    def do_processing(self, **kwargs):
+        if (len(self.params['vld_imgs']) != len(self.params['clrsky_imgs'])) and (len(self.params['vld_imgs']) != len(self.params['sref_imgs'])):
+            raise Exception("There are different numbers of valid masks, clear sky masks and SREF images.")
+        rsgis_utils = rsgislib.RSGISPyUtils()
+        
+        if len(self.params['vld_imgs']) > 0:
+            granule_vld_img = os.path.join(self.params['tmp_dir'], "{}_vld_img.kea".format(self.params['granule']))
+            rsgislib.imagecalc.calcMultiImgBandStats(self.params['vld_imgs'], granule_vld_img, rsgislib.SUMTYPE_MAX, "KEA", rsgislib.TYPE_8UINT, 0, False)
+
+            granule_clearsky_img = os.path.join(self.params['tmp_dir'], "{}_clearsky_img.kea".format(self.params['granule']))
+            rsgislib.imagecalc.calcMultiImgBandStats(self.params['clrsky_imgs'], granule_clearsky_img, rsgislib.SUMTYPE_MAX, "KEA", rsgislib.TYPE_8UINT, 0, False)
+
+            scn_veg_msks = list()
+            n_imgs = len(self.params['sref_imgs'])
+            for n in range(n_imgs):
+                sref_img = self.params['sref_imgs'][n]
+                clrsky_img = self.params['clrsky_imgs'][n]
+                print(sref_img)
+                basename = rsgis_utils.get_file_basename(sref_img)
+                scn_ndvi_img = os.path.join(self.params['tmp_dir'], "{}_ndvi.kea".format(basename))
+
+                rBand = 3
+                nBand = 7
+                rsgislib.imagecalc.calcindices.calcNDVI(sref_img, rBand, nBand, scn_ndvi_img, stats=False, gdalformat='KEA')
+
+                scn_veg_img = os.path.join(self.params['tmp_dir'], "{}_veg.kea".format(basename))
+                band_defs = [rsgislib.imagecalc.BandDefn('ndvi', scn_ndvi_img, 1),
+                             rsgislib.imagecalc.BandDefn('vld', clrsky_img, 1)]
+                exp = '(ndvi>0.2) && (vld == 1)?1:0'
+                rsgislib.imagecalc.bandMath(scn_veg_img, exp, 'KEA', rsgislib.TYPE_8UINT, band_defs)
+                rsgislib.rastergis.populateStats(scn_veg_img, addclrtab=True, calcpyramids=True, ignorezero=True)
+                scn_veg_msks.append(scn_veg_img)
+
+            rsgislib.imagecalc.calcMultiImgBandStats(scn_veg_msks, self.params['granule_out_img_file'], rsgislib.SUMTYPE_MAX, "KEA", rsgislib.TYPE_8UINT, 0, False)
+            rsgislib.rastergis.populateStats(self.params['granule_out_img_file'], addclrtab=True, calcpyramids=True, ignorezero=True)
+
+            rsgislib.vectorutils.polygoniseRaster2VecLyr(self.params['granule_out_vec_file'], self.params['granule_out_lyr'], 'GPKG', self.params['granule_out_img_file'], imgBandNo=1, maskImg=self.params['granule_out_img_file'], imgMaskBandNo=1, replace_file=True, replace_lyr=True, pxl_val_fieldname='PXLVAL')
+
+            n_feats = rsgislib.vectorutils.getVecFeatCount(self.params['granule_out_vec_file'], self.params['granule_out_lyr'])
+            granule_names = list()
+            for i in range(n_feats):
+                granule_names.append(self.params['granule'])
+            rsgislib.vectorutils.writeVecColumn(self.params['granule_out_vec_file'], self.params['granule_out_lyr'], 'Granule', ogr.OFTString, granule_names)
+
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+
+    def required_fields(self, **kwargs):
+        return ["granule", "vld_imgs", "clrsky_imgs", "sref_imgs", "granule_out_lyr", "granule_out_vec_file", "granule_out_img_file", "tmp_dir"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['granule_out_img_file']] = 'gdal_image'
+        files_dict[self.params['granule_out_vec_file']] = 'gdal_vector'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Reset the tmp dir
+        if os.path.exists(self.params['tmp_dir']):
+            shutil.rmtree(self.params['tmp_dir'])
+        os.mkdir(self.params['tmp_dir'])
+
+        # Remove the output file.
+        if os.path.exists(self.params['granule_out_vec_file']):
+            os.remove(self.params['granule_out_vec_file'])
+        if os.path.exists(self.params['granule_out_img_file']):
+            os.remove(self.params['granule_out_img_file'])
+
+if __name__ == "__main__":
+    CreateGranuleVegMsk().std_run()
+
+
---
+++ 02_sen2_non_veg_lyr/01_apply_scn_thresholds/simplified/gen_granule_veg_msks_simplified.py
@@ -0,0 +1,88 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import rsgislib
+import logging
+import os
+import sys
+
+sys.path.insert(0, "../../01_sen2_ard/03_find_dwnld_scns")
+from sen2scnprocess import RecordSen2Process
+
+logger = logging.getLogger(__name__)
+
+class GenVegGranuleMsk(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs['scn_db_file']):
+            raise Exception("Sentinel-2 scene database does not exist...")
+
+        rsgis_utils = rsgislib.RSGISPyUtils()
+        granule_lst = rsgis_utils.readTextFile2List(kwargs['granule_lst'])
+
+        sen2_rcd_obj = RecordSen2Process(kwargs['scn_db_file'])
+        err_scns = []
+        for granule in granule_lst:
+            print(granule)
+            granule_out_img_file = os.path.join(kwargs['granule_out_img_path'], "{}_veg.kea".format(granule))
+            granule_out_vec_file = os.path.join(kwargs['granule_out_vec_path'], "{}_veg.gpkg".format(granule))
+            if (not os.path.exists(granule_out_img_file)) or (not os.path.exists(granule_out_vec_file)):
+                scns = sen2_rcd_obj.granule_scns(granule)
+                vld_imgs = list()
+                clrsky_imgs = list()
+                sref_imgs = list()
+                for scn in scns:
+                    print("\t{}".format(scn.product_id))
+                    if scn.ard:
+                        vld_img = self.find_first_file(scn.ard_path, "*valid.kea", rtn_except=False)
+                        clrsky_img = self.find_first_file(scn.ard_path, "*clearsky_refine.kea", rtn_except=False)
+                        sref_img = self.find_first_file(scn.ard_path, "*vmsk_rad_srefdem_stdsref.kea", rtn_except=False)
+                        if (vld_img is None) or (clrsky_img is None) or (sref_img is None):
+                            clouds_img = self.find_first_file(scn.ard_path, "*clouds.kea", rtn_except=False)
+                            if clouds_img is None:
+                                print("***ERROR***: {}".format(scn.ard_path))
+                                err_scns.append(scn.ard_path)
+                        else:
+                            vld_imgs.append(vld_img)
+                            clrsky_imgs.append(clrsky_img)
+                            sref_imgs.append(sref_img)
+                if (len(vld_imgs) > 0) and (len(clrsky_imgs) > 0) and (len(sref_imgs) > 0):
+                    c_dict = dict()
+                    c_dict['granule'] = granule
+                    c_dict['vld_imgs'] = vld_imgs
+                    c_dict['clrsky_imgs'] = clrsky_imgs
+                    c_dict['sref_imgs'] = sref_imgs
+                    c_dict['granule_out_lyr'] = granule
+                    c_dict['granule_out_vec_file'] = granule_out_vec_file
+                    c_dict['granule_out_img_file'] = granule_out_img_file
+                    c_dict['tmp_dir'] = os.path.join(kwargs['tmp_dir'], "{}_granule_veg_msk".format(granule))
+                    if not os.path.exists(c_dict['tmp_dir']):
+                        os.mkdir(c_dict['tmp_dir'])
+                    self.params.append(c_dict)
+        print("ERRORS:")
+        for err_scn in err_scns:
+            print(err_scn)
+
+    def run_gen_commands(self):
+        self.gen_command_info(scn_db_file='/scratch/a.pfb/gmw_v2_gapfill/scripts/01_sen2_ard/03_find_dwnld_scns/sen2_scn.db',
+                              granule_lst='/scratch/a.pfb/gmw_v2_gapfill/scripts/01_sen2_ard/sen2_roi_granule_lst.txt',
+                              granule_out_img_path='/scratch/a.pfb/gmw_v2_gapfill/data/granule_vegmsks_imgs',
+                              granule_out_vec_path='/scratch/a.pfb/gmw_v2_gapfill/data/granule_vegmsks_vecs',
+                              tmp_dir='/scratch/a.pfb/gmw_v2_gapfill/tmp')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("sen2_granule_veg", 16448, '/scratch/a.pfb/gmw_v2_gapfill/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("create_granule_veg_msk.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'create_granule_veg_msk'
+    process_tools_cls = 'CreateGranuleVegMsk'
+
+    create_tools = GenVegGranuleMsk(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                        lock_file_path="./gmw_gapfill_lock_file.txt",
+                                        process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
