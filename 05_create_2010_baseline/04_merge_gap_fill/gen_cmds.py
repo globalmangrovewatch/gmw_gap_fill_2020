e01d578761bd5df0c69a968fcdd3488d67dcc32c
Pete Bunting <petebunting@mac.com>
2021-04-08 15:32:14 +0100

    Add scripts to merge the qa edits and sen2 gap fill to create a single layer.

---
+++ 05_create_2010_baseline/04_merge_gap_fill/gen_cmds.py
@@ -0,0 +1,52 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+import glob
+import pathlib
+
+logger = logging.getLogger(__name__)
+
+class GenCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        tiles = glob.glob(kwargs['ref_tiles_path'])
+        for tile in tiles:
+            tile_basename = self.get_file_basename(tile)
+
+            qa_xtrs_img = os.path.join(kwargs['qaedits_dir'], '{}_QAXtrs.kea'.format(tile_basename))
+            gapfill_img = os.path.join(kwargs['gapfill_dir'], '{}_GFBase.kea'.format(tile_basename))
+
+            out_file = os.path.join(kwargs['out_dir'], '{}_gmw_gapfill_fnl.kea'.format(tile_basename))
+
+            if not os.path.exists(out_file):
+                c_dict = dict()
+                c_dict['tile_img'] = tile
+                c_dict['qa_xtrs_img'] = qa_xtrs_img
+                c_dict['gapfill_img'] = gapfill_img
+                c_dict['out_file'] = out_file
+                self.params.append(c_dict)
+
+
+    def run_gen_commands(self):
+        self.gen_command_info(ref_tiles_path='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_ref_tiles/*.kea',
+                              gapfill_dir='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_gapfill_base',
+                              qaedits_dir='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_qa_xtrs_base',
+                              out_dir='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_gapfill_edits')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("merge_gmw_edits", 16448, '/scratch/a.pfb/gmw_v2_gapfill/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("merge_gmw_edits.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'merge_gmw_edits'
+    process_tools_cls = 'MergeGMWEdits'
+
+    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_gapfill_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
---
+++ 05_create_2010_baseline/04_merge_gap_fill/merge_gmw_edits.py
@@ -0,0 +1,42 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import shutil
+import rsgislib
+import rsgislib.imagecalc
+import rsgislib.rastergis
+
+logger = logging.getLogger(__name__)
+
+
+class MergeGMWEdits(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='merge_gmw_edits.py', descript=None)
+
+    def do_processing(self, **kwargs):
+
+        bandDefns = []
+        bandDefns.append(rsgislib.imagecalc.BandDefn('qa', self.params['qa_xtrs_img'], 1))
+        bandDefns.append(rsgislib.imagecalc.BandDefn('gfill', self.params['gapfill_img'], 1))
+        rsgislib.imagecalc.bandMath(self.params['out_file'], '(qa==1)||(gfill==1)?1:0', 'KEA', rsgislib.TYPE_8UINT, bandDefns)
+        rsgislib.rastergis.populateStats(self.params['out_file'], addclrtab=True, calcpyramids=True, ignorezero=True)
+
+
+    def required_fields(self, **kwargs):
+        return ["tile_img", "qa_xtrs_img", "gapfill_img", "out_file"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_file']] = 'gdalimg'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output file.
+        if os.path.exists(self.params['out_file']):
+            os.remove(self.params['out_file'])
+
+if __name__ == "__main__":
+    MergeGMWEdits().std_run()
+
+
