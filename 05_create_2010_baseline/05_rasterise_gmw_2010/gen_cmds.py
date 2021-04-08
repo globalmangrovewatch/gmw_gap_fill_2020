24721b302866f561e2e73a081725137a5aad64cb
Pete Bunting <petebunting@mac.com>
2021-04-08 13:21:56 +0100

    Add scripts to rasterise the GMW 2010 layer.

---
+++ 05_create_2010_baseline/05_rasterise_gmw_2010/gen_cmds.py
@@ -0,0 +1,48 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+import logging
+import os
+import glob
+import pathlib
+
+logger = logging.getLogger(__name__)
+
+class GenCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        tiles = glob.glob(kwargs['ref_tiles_path'])
+        for tile in tiles:
+            tile_basename = self.get_file_basename(tile)
+
+            out_file = os.path.join(kwargs['out_dir'], '{}_GFBase.kea'.format(tile_basename))
+            if not os.path.exists(out_file):
+                c_dict = dict()
+                c_dict['tile_img'] = tile
+                c_dict['gmw_vec'] = kwargs['gmw_vec']
+                c_dict['gmw2010_lyr'] = kwargs['gmw2010_lyr']
+                c_dict['out_file'] = out_file
+                self.params.append(c_dict)
+
+
+    def run_gen_commands(self):
+        self.gen_command_info(ref_tiles_path='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_ref_tiles/*.kea',
+                              gmw_vec='/scratch/a.pfb/gmw_v2_gapfill/data/GMW_MangroveExtent_WGS84_v2.0.gpkg',
+                              gmw2010_lyr='gmw2010v2.0',
+                              out_dir='/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_gapfill_base')
+        self.pop_params_db()
+        self.create_slurm_sub_sh("rasterise_gapfill", 16448, '/scratch/a.pfb/gmw_v2_gapfill/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file=None, account_name='scw1376', n_cores_per_job=10, n_jobs=10,
+                                 job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("rasterise_gmw2010.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'rasterise_gmw2010'
+    process_tools_cls = 'RasteriseGMW2010'
+
+    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_gapfill_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
---
+++ 05_create_2010_baseline/05_rasterise_gmw_2010/rasterise_gmw2010.py
@@ -0,0 +1,38 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import rsgislib
+import rsgislib.vectorutils
+
+logger = logging.getLogger(__name__)
+
+
+class RasteriseGMW2010(PBPTQProcessTool):
+
+    def __init__(self):
+        super().__init__(cmd_name='rasterise_gmw2010.py', descript=None)
+
+    def do_processing(self, **kwargs):
+
+        rsgislib.vectorutils.rasteriseVecLyr(self.params['gmw_vec'], self.params['gmw2010_lyr'],
+                                             self.params['tile_img'], self.params['out_file'], gdalformat="KEA",
+                                             burnVal=1, datatype=rsgislib.TYPE_8UINT, vecAtt=None, vecExt=False,
+                                             thematic=True, nodata=0)
+
+    def required_fields(self, **kwargs):
+        return ["tile_img", "gmw_vec", "gmw2010_lyr", "out_file"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params['out_file']] = 'gdalimg'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        # Remove the output file.
+        if os.path.exists(self.params['out_file']):
+            os.remove(self.params['out_file'])
+
+if __name__ == "__main__":
+    RasteriseGMW2010().std_run()
+
+
