70065016af6a80c81cb56fd1eca79642727a7ee7
Pete Bunting <petebunting@mac.com>
2021-05-15 17:57:43 +0100

    Update paths for merging vector layers.

--- 05_create_2010_baseline/19_vectorise_init_qa_gmw_v3_baseline/merge_gmw_v3_tiles.py
+++ 05_create_2010_baseline/19_vectorise_init_qa_gmw_v3_baseline/merge_gmw_v3_tiles.py
@@ -1,8 +1,8 @@
 import glob
 import rsgislib.vectorutils
 
-input_vecs = glob.glob("/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_qa_vec/*.gpkg")
+input_vecs = glob.glob("/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_further_qa_part2_vec/*.gpkg")
 
-out_file = "/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_baseline_up1.gpkg"
+out_file = "/scratch/a.pfb/gmw_v2_gapfill/data/gmw_tiles/gmw_init_v3_baseline_up2.gpkg"
 
 rsgislib.vectorutils.mergeVectors2GPKG(input_vecs, out_file, "gmw_init_v3", False)
