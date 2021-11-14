8657a45922b868e29e14553ba693740fc2ba246b
Pete Bunting <petebunting@mac.com>
2021-11-14 22:46:45 +0000

    Add scripts to split the accuracy points into sets for testing the number accuracy points required.

---
+++ 06_v25_accuracy_assessment/05_create_acc_pt_sets/create_acc_pt_sets.py
@@ -0,0 +1,20 @@
+
+import os
+import glob
+import rsgislib.tools.filetools
+import rsgislib.vectorutils
+
+vec_files = glob.glob("/Users/pete/Temp/gmw_v25_extent/roi_cls_acc_pts/*.gpkg")
+out_base_dir = "/Users/pete/Temp/gmw_v25_extent/roi_cls_acc_pts_sets"
+
+for vec_file in vec_files:
+    print(vec_file)
+    out_vec_lyr = vec_lyr = rsgislib.vectorutils.get_vec_lyrs_lst(vec_file)[0]
+    basename = rsgislib.tools.filetools.get_file_basename(vec_file)
+    out_dir = os.path.join(out_base_dir, basename)
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    
+    out_vec_file_base = os.path.join(out_dir, "{}_".format(basename))
+    rsgislib.vectorutils.create_acc_pt_sets(vec_file, vec_lyr, out_vec_file_base, out_vec_lyr, "gmw_v25_cls", 20, sets_col="set_id", out_format='GPKG', out_ext='gpkg', rnd_seed=42)
+    
