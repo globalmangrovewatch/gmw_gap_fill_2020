8424f0ae977e698c2b433cb8be3638f3e908784c
Pete Bunting <petebunting@mac.com>
2021-11-15 15:54:30 +0000

    Bug fix.

--- 06_v25_accuracy_assessment/07_extract_ref_imgs/create_palsar2010_imgs.py
+++ 06_v25_accuracy_assessment/07_extract_ref_imgs/create_palsar2010_imgs.py
@@ -16,6 +16,6 @@ vec_file = "../01_define_rois/roi_centre_bboxs_roi_ids.geojson"
 vec_lyr = "roi_centre_bboxs_roi_ids"
 out_img_base = "/bigdata/gmw_v25_acc_ass/palsar10_acc_rois/gmw_ls_acc_roi_"
 
-rsgislib.imageutils.subset_to_geoms_bboxs(vrt_glb_img, vec_file, vec_lyr, "roi_id", out_img_base, "GTIFF", rsgislib.TYPE_8UINT, "tif")
+rsgislib.imageutils.subset_to_geoms_bbox(vrt_glb_img, vec_file, vec_lyr, "roi_id", out_img_base, "GTIFF", rsgislib.TYPE_8UINT, "tif")
 
 
--- 06_v25_accuracy_assessment/07_extract_ref_imgs/run.sh
+++ 06_v25_accuracy_assessment/07_extract_ref_imgs/run.sh
@@ -1,1 +1,3 @@
-singularity exec --bind /mangroves_server:/mangroves_server --bind /bigdata:/bigdata /bigdata/gmw_v25_acc_ass/sw_imgs/au-eoed-beta-dev.sif python create_fnl_outs.py
+singularity exec --bind /mangroves_server:/mangroves_server --bind /bigdata:/bigdata /bigdata/gmw_v25_acc_ass/sw_imgs/au-eoed-beta-dev.sif python /bigdata/gmw_v25_acc_ass/scripts/06_v25_accuracy_assessment/07_extract_ref_imgs/create_ls2010_imgs.py
+
+singularity exec --bind /mangroves_server:/mangroves_server --bind /bigdata:/bigdata /bigdata/gmw_v25_acc_ass/sw_imgs/au-eoed-beta-dev.sif python /bigdata/gmw_v25_acc_ass/scripts/06_v25_accuracy_assessment/07_extract_ref_imgs/create_palsar2010_imgs.py
