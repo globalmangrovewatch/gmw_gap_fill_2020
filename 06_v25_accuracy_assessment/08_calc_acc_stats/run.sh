81ad774853c1adec499a2d74482f08d816f42d84
Pete Bunting <petebunting@mac.com>
2021-11-19 11:51:15 +0000

    Update adding more checked accuracy assessment points.

--- 06_v25_accuracy_assessment/08_calc_acc_stats/run.sh
+++ 06_v25_accuracy_assessment/08_calc_acc_stats/run.sh
@@ -1,9 +1,13 @@
-#export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/pete/Temp/rsgislib_lcl/lib
-#export PYTHONPATH=$PYTHONPATH:/Users/pete/Temp/rsgislib_lcl/lib/python3.9/site-packages
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/pete/Temp/rsgislib_lcl/lib
+export PYTHONPATH=$PYTHONPATH:/Users/pete/Temp/rsgislib_lcl/lib/python3.9/site-packages
 
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/pete/Temp/rsgislib_v5_dev_install/lib
-export PYTHONPATH=$PYTHONPATH:/Users/pete/Temp/rsgislib_v5_dev_install/lib/python3.9/site-packages
+#export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/pete/Temp/rsgislib_v5_dev_install/lib
+#export PYTHONPATH=$PYTHONPATH:/Users/pete/Temp/rsgislib_v5_dev_install/lib/python3.9/site-packages
 
-python calc_acc_stats.py
+#python calc_acc_stats_01.py
+#python calc_acc_stats_02.py
 
+
+
+python calc_acc_stats_12.py
 
