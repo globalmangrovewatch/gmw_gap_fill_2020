1379f0a8d0ae47df815ee32b32a009fc751b9241
Pete Bunting <petebunting@mac.com>
2021-11-22 22:13:49 +0000

    Update with more accuracy assessment points.

--- 06_v25_accuracy_assessment/08_calc_acc_stats/run.sh
+++ 06_v25_accuracy_assessment/08_calc_acc_stats/run.sh
@@ -10,7 +10,10 @@ export PYTHONPATH=$PYTHONPATH:/Users/pete/Temp/rsgislib_lcl/lib/python3.9/site-p
 #python calc_acc_stats_04.py
 #python calc_acc_stats_05.py
 #python calc_acc_stats_06.py
-python calc_acc_stats_07.py
-
+#python calc_acc_stats_07.py
+#python calc_acc_stats_08.py
+#python calc_acc_stats_09.py
+#python calc_acc_stats_10.py
+python calc_acc_stats_11.py
 #python calc_acc_stats_12.py
 
