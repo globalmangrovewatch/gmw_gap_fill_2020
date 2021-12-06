c545b0c41a2d88240ef91439a83045ecc89610fb
Pete Bunting <petebunting@mac.com>
2021-12-06 21:22:41 +0000

    Update stats files.

--- 06_v25_accuracy_assessment/13_calc_v2_acc_stats/calc_v25_acc_stats.py
+++ 06_v25_accuracy_assessment/13_calc_v2_acc_stats/calc_v25_acc_stats.py
@@ -19,18 +19,18 @@ v2_comp_sites = list()
 v2_comp_sites_json = list()
 
 ref_col = "ref_cls"
-cls_col = "gmw_v2_cls"
+cls_col = "gmw_v25_cls"
 
-out_set_dir = "set_v2_accs"
+out_set_dir = "set_v25_accs"
 acc_vec_dir = "../10_calc_set_acc_stats/set_acc_stats/"
 for i in range(60):
     vec_file = os.path.join(acc_vec_dir, "gmw_v25_set_{}_acc_pts.geojson".format(i+1))
     if os.path.exists(vec_file):
         vec_lyr = rsgislib.vectorutils.get_vec_lyrs_lst(vec_file)[0]
         
-        acc_stats_json_file = os.path.join(out_set_dir, "gmw_v2_set_{}_acc_stats.json".format(i+1))
-        acc_stats_csv_file = os.path.join(out_set_dir, "gmw_v2_set_{}_acc_stats.csv".format(i+1))
-        acc_conf_stats_json_file = os.path.join(out_set_dir, "gmw_v2_set_{}_acc_conf_stats.json".format(i+1))
+        acc_stats_json_file = os.path.join(out_set_dir, "gmw_v25_set_{}_acc_stats.json".format(i+1))
+        acc_stats_csv_file = os.path.join(out_set_dir, "gmw_v25_set_{}_acc_stats.csv".format(i+1))
+        acc_conf_stats_json_file = os.path.join(out_set_dir, "gmw_v25_set_{}_acc_conf_stats.json".format(i+1))
         
         if (i+1) in v2_comp_site_idxs:
             v2_comp_sites.append({"file": vec_file, "layer": vec_lyr})
@@ -38,31 +38,31 @@ for i in range(60):
         all_sites.append({"file": vec_file, "layer": vec_lyr})
         all_sites_json.append(acc_stats_json_file)
         
-        #calc_acc_stats(vec_file, vec_lyr, ref_col, cls_col, acc_stats_json_file, acc_stats_csv_file, acc_conf_stats_json_file)
+        calc_acc_stats(vec_file, vec_lyr, ref_col, cls_col, acc_stats_json_file, acc_stats_csv_file, acc_conf_stats_json_file)
 
 
 vec_acc_pts_file = "gmw_v25_all_acc_pts.geojson"
 vec_acc_pts_lyr = "gmw_v25_all_acc_pts"
 rsgislib.vectorutils.merge_vector_layers(all_sites, out_vec_file=vec_acc_pts_file, out_vec_lyr=vec_acc_pts_lyr, out_format="GeoJSON")
 
-acc_stats_json_file = "gmw_v2_all_acc_stats.json"
-acc_stats_csv_file = "gmw_v2_all_acc_stats.csv"
-acc_conf_stats_json_file = "gmw_v2_set_all_acc_conf_stats.json"
+acc_stats_json_file = "gmw_v25_all_acc_stats.json"
+acc_stats_csv_file = "gmw_v25_all_acc_stats.csv"
+acc_conf_stats_json_file = "gmw_v25_set_all_acc_conf_stats.json"
 
 calc_acc_stats(vec_acc_pts_file, vec_acc_pts_lyr, ref_col, cls_col, acc_stats_json_file, acc_stats_csv_file, acc_conf_stats_json_file)
 
-out_acc_json_sum_file = "gmw_v2_all_acc_stats_set_sum.json"
+out_acc_json_sum_file = "gmw_v25_all_acc_stats_set_sum.json"
 rsgislib.classification.classaccuracymetrics.summarise_multi_acc_ptonly_metrics(all_sites_json, out_acc_json_sum_file)
 
 vec_acc_pts_file = "gmw_v25_v2_comp_acc_pts.geojson"
 vec_acc_pts_lyr = "gmw_v25_v2_comp_acc_pts"
 rsgislib.vectorutils.merge_vector_layers(v2_comp_sites, out_vec_file=vec_acc_pts_file, out_vec_lyr=vec_acc_pts_lyr, out_format="GeoJSON")
 
-acc_stats_json_file = "gmw_v2_comp_acc_stats.json"
-acc_stats_csv_file = "gmw_v2_comp_acc_stats.csv"
-acc_conf_stats_json_file = "gmw_v2_comp_acc_conf_stats.json"
+acc_stats_json_file = "gmw_v25_comp_acc_stats.json"
+acc_stats_csv_file = "gmw_v25_comp_acc_stats.csv"
+acc_conf_stats_json_file = "gmw_v25_comp_acc_conf_stats.json"
 
 calc_acc_stats(vec_acc_pts_file, vec_acc_pts_lyr, ref_col, cls_col, acc_stats_json_file, acc_stats_csv_file, acc_conf_stats_json_file)
 
-out_acc_json_sum_file = "gmw_v2_comp_acc_stats_set_sum.json"
+out_acc_json_sum_file = "gmw_v25_comp_acc_stats_set_sum.json"
 rsgislib.classification.classaccuracymetrics.summarise_multi_acc_ptonly_metrics(v2_comp_sites_json, out_acc_json_sum_file)
--- 06_v25_accuracy_assessment/13_calc_v2_acc_stats/run.sh
+++ 06_v25_accuracy_assessment/13_calc_v2_acc_stats/run.sh
@@ -2,6 +2,7 @@ export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/Users/pete/Temp/rsgislib_lcl/lib
 export PYTHONPATH=$PYTHONPATH:/Users/pete/Temp/rsgislib_lcl/lib/python3.9/site-packages
 
 
-python calc_v2_acc_stats.py
+#python calc_v2_acc_stats.py
 
+python calc_v25_acc_stats.py
 
